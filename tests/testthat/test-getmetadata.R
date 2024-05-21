a7234c602836297eb6f91e17261fddee0359c133
Hough <caroline.hough@dhsc.gov.uk>
2024-05-21 15:24:51 +0100

    QA recommended running unit tests with multiple URLS - have added in 3 more URLs to test that valid url generates right output. Needed to extend sys.sleep from 1 to 4 to overcome rate-limits set by ONS website.

--- tests/testthat/test-getmetadata.R
+++ tests/testthat/test-getmetadata.R
@@ -5,15 +5,53 @@ test_that("valid url generates right output", {
     "infectionsurveydata"
   )
 
+  ons_url2 <- paste0(
+    "https://www.ons.gov.uk/employmentandlabourmarket/peopleinwork/",
+    "labourproductivity/datasets/unitlabourcostandunitwagecosttimeseries"
+  )
+
+  ons_url3 <- paste0(
+    "https://www.ons.gov.uk/peoplepopulationandcommunity/",
+    "birthsdeathsandmarriages/deaths/datasets/",
+    "childmortalitystatisticschildhoodinfantandperinatalchildhoodinfantand",
+    "perinatalmortalityinenglandandwales"
+  )
+
+  ons_url4 <- paste0(
+    "https://www.ons.gov.uk/economy/nationalaccounts/balanceofpayments/datasets/",
+    "tradeingoodsmretsallbopeu2013timeseriesspreadsheet"
+  )
+
   element_info <- "h1.page-intro__title"
 
-  Sys.sleep(1)
+  Sys.sleep(4)
 
   df <- get_html_text(ons_url, element_info)
 
+  Sys.sleep(4)
+
+  df2 <- get_html_text(ons_url2, element_info)
+
+  Sys.sleep(4)
+
+  df3 <- get_html_text(ons_url3, element_info)
+
+  Sys.sleep(4)
+
+  df4 <- get_html_text(ons_url4, element_info)
+
+  Sys.sleep(4)
+
   result <- "Dataset Coronavirus (COVID-19) Infection Survey: England"
+  result2 <- "Dataset Unit labour cost and unit wage cost time series"
+  result3 <- "Dataset Child mortality (death cohort) tables in England and Wales"
+  result4 <- "Dataset UK trade time series"
 
   expect_equal(df, result)
+  expect_equal(df2, result2)
+  expect_equal(df3, result3)
+  expect_equal(df4, result4)
+
 })
 
 test_that("broken url generates error", {
